cf53a5591002f4be087e7dc7c8a09eac41a4ddbb
PlusUltraCode <kkd06144@naver.com>
2025-03-27 15:52:51 +0900

    docker  레디스 추가본

--- src/main/java/com/example/instagram_server2/AppController.java
+++ src/main/java/com/example/instagram_server2/AppController.java
@@ -1,6 +1,7 @@
 package com.example.instagram_server2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,9 @@ public class AppController {
     @Autowired
     private JdbcTemplate jdbcTemplate; // JdbcTemplate 사용
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate; // RedisTemplate 사용
+
     @GetMapping("/")
     public String index() {
         // MySQL 연결 확인
@@ -19,6 +23,25 @@ public class AppController {
             return "Successfully connected to MySQL!";
         } catch (Exception e) {
             return "Failed to connect to MySQL: " + e.getMessage();
+        }
+    }
+
+    @GetMapping("/redis")
+    public String checkRedisConnection() {
+        // Redis 연결 확인
+        try {
+            // Redis에 간단한 값을 저장하고 읽어오기
+            redisTemplate.opsForValue().set("testKey", "Hello, Redis!");
+            String redisValue = redisTemplate.opsForValue().get("testKey");
+
+            // Redis에 접근이 성공하면 값을 읽어온 후, 연결 성공 메시지 반환
+            if ("Hello, Redis!".equals(redisValue)) {
+                return "Successfully connected to Redis!";
+            } else {
+                return "Failed to connect to Redis!";
+            }
+        } catch (Exception e) {
+            return "Failed to connect to Redis: " + e.getMessage();
         }
     }
 }
