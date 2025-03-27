02d1f5bbb3f9790be70c3aab84a92f0bcc2c7212
PlusUltraCode <kkd06144@naver.com>
2025-03-27 15:21:22 +0900

    docker  제발 ㅈ되라

--- src/main/java/com/example/instagram_server2/AppController.java
+++ src/main/java/com/example/instagram_server2/AppController.java
@@ -1,14 +1,24 @@
 package com.example.instagram_server2;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate; // JdbcTemplate 사용
+
     @GetMapping("/")
     public String index() {
-        return "FUCK, world!";
+        // MySQL 연결 확인
+        try {
+            jdbcTemplate.execute("SELECT 1"); // MySQL 연결을 확인하기 위한 간단한 쿼리
+            return "Successfully connected to MySQL!";
+        } catch (Exception e) {
+            return "Failed to connect to MySQL: " + e.getMessage();
+        }
     }
-
 }
